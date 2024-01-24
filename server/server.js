a34123ce5c0ce3c502e59fef50b9bdd785598838
Sean Worrell <spworrell@gmail.com>
2024-01-23 22:00:01 -0500

    exp working

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+// host public react
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 app.listen(3000);
