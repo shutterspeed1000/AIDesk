cfdf2c27eefea34d565b5352ece74d0470beced0
Sean Worrell <spworrell@gmail.com>
2024-02-22 21:47:48 -0500

    single page made

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,8 +1,6 @@
 function Header() {
   return (
     <div id="headsize">
-      <img src="cscf-logo.png" id="cscf" />
-
       <h1 id="title">CSCF AI Helpdesk System</h1>
     </div>
   );
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,25 +1,28 @@
 function Main() {
   return (
     <div>
-      <lablel>Please select the technology you need assistance with.</lablel>
-      <input type="radio" id="laptop" name="laptop" value="laptop"></input>
-      <label for="laptop">Laptop</label>
-      <input type="radio" id="laptop" name="laptop" value="laptop"></input>
-      <label for="laptop">Laptop</label>
-      <input type="radio" id="laptop" name="laptop" value="laptop"></input>
-      <label for="laptop">Laptop</label>
-      <input type="radio" id="laptop" name="laptop" value="laptop"></input>
-      <label for="laptop">Laptop</label>
-      <br></br>
-
-      <textarea></textarea>
-
       <p id="directions">
         Please complete the form to the left to ask our AI system for
         assistance. It will provide you with a possable solution to your issue.
         If the solution does not work, the AI will open a ticket in the CSCF
         ticketing system for further assitance.
       </p>
+      <lablel>Please select the technology you need assistance with.</lablel>
+      <br></br>
+      <select name="cars" id="cars">
+        <option value="laptop">Laptop</option>
+        <option value="application">Application</option>
+        <option value="website">Website</option>
+        <option value="other">Other</option>
+      </select>
+
+      <br></br>
+      <p>Please enter a brief description of the issue you are experiencing.</p>
+      <textarea rows="8" cols="80"></textarea>
+
+      <br></br>
+
+      <button id="submit">Submit</button>
     </div>
   );
 }
